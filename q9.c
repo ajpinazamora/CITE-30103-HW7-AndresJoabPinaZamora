b16060a5b86d7e0a2014fc6893d3a0eb9de887c1
ajpinazamora <ajpinazamora@gmail.com>
2025-11-24 21:03:50 -0600

    Array reverser

---
+++ q9.c
@@ -0,0 +1,21 @@
+/* question9.c */
+#include <stdio.h>
+
+int main(void) {
+    int arr[5];
+    for (int i = 0; i < 5; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for (int i = 0, j = 4; i < j; i++, j--) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    for (int i = 0; i < 5; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
+
