611753179686a5453b6504934bb9dfa961dd859f
ajpinazamora <ajpinazamora@gmail.com>
2025-11-24 20:49:51 -0600

    Larges and Smallest Finder

---
+++ q7.c
@@ -0,0 +1,20 @@
+/* question7.c */
+#include <stdio.h>
+
+int main(void) {
+    int arr[10];
+    for (int i = 0; i < 10; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int max = arr[0], min = arr[0];
+    for (int i = 1; i < 10; i++) {
+        if (arr[i] > max) max = arr[i];
+        if (arr[i] < min) min = arr[i];
+    }
+
+    printf("Largest = %d\n", max);
+    printf("Smallest = %d\n", min);
+    return 0;
+}
+
